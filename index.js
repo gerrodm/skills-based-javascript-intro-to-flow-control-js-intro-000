0ba297465cf2419fb33c22c238348329ec776cad
gerrod <gerrodmilton55@gmail.com>
2016-11-01 18:03:05 +0000

    new message

---
+++ index.js
@@ -0,0 +1,34 @@
+// write your code below!
+
+function basicTeenager(age) {
+    if ( age >= 13 && age <= 19) {
+      return "You are a teenager!";
+    }else{
+        return undefined;
+      }
+}
+
+function teenager(age) {
+   if(age >= 13 && age <= 19){
+     return 'You are a teenager!';
+   }else{
+     return "You are not a teenager";
+   }
+   }
+
+
+function ageChecker(age) {
+  if(age >= 12){
+  return "You are a teenager!";
+  }else{
+    return "You are a kid";
+}
+}
+
+function ternaryTeenager(age) {
+   return 'You are a teenager'
+}
+
+function switchAge(age) {
+   return 'You are a teenager'
+}
